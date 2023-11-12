8b744d56d5b7f86010369c541c01573d53534a2e
orozco92 <goldstein.david92@gmail.com>
2023-11-12 16:03:25 -0500

    * Tree builder interface and config * List tree builder * List tree builder tests

---
+++ src/factory/list-tree-builder.ts
@@ -0,0 +1,35 @@
+import { Tree } from "../models/tree";
+import * as _ from "lodash";
+import { TreeBuilderConfig, TreeBuilderInterface } from "../models/tree-builder";
+/**
+ * Converts a list into a tree
+ */
+export class ListTreeBuilder<E = unknown> implements TreeBuilderInterface<E> {
+  /**
+   * @param opts options to apply
+   * @param data tree source data
+   * @returns resulting tree
+   */
+  buildTree(opts: TreeBuilderConfig, data: E[]): Tree<E> {
+    const idKey = opts.pk || "id";
+    if (!opts.fk || "string" !== typeof opts.fk) {
+      throw new Error("foreign (opts.fk) key must be provided");
+    }
+    let root;
+    const list = _.chain(data)
+      .map((item) => new Tree(item[idKey], item))
+      .value();
+    const idGroup = _.chain(list)
+      .groupBy(idKey)
+      .mapValues((item) => item[0])
+      .value();
+    list.forEach((item) => {
+      if (item.data[opts.fk]) {
+        idGroup[item.data[opts.fk]].addChild(item);
+      } else {
+        root = item;
+      }
+    });
+    return root;
+  }
+}
